862ecee2284ef85ca8ed39e2f5a1d3a9a9e632d5
noah smith <noahjosephsmith@gmail.com>
2021-11-23 17:07:46 -0700

    first time node

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-
-console.log(document)
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+  };
